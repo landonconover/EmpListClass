4f6df010696d12af78bac97ac40c1c1661a678ef
[Landon Conover] <[landon.conover@gmail.com]>
2020-03-11 13:41:55 -0600

    all of the code

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,57 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <employee-form @add:employee="addEmployee" />
+    <employee-table :employees="employees" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import EmployeeTable from '@/components/EmployeeTable.vue'
+import EmployeeForm from '@/components/EmployeeForm.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    EmployeeTable,
+    EmployeeForm
+  },
+  data() {
+    return {
+      employees: [
+        {
+          id: 1,
+          name: 'Richard Hendricks',
+          email: 'richard@piedpiper.com',
+        },
+        {
+          id: 2,
+          name: 'Bertram Gilfoyle',
+          email: 'gilfoyle@piedpiper.com',
+        },
+        {
+          id: 3,
+          name: 'Dinesh Chugtai',
+          email: 'dinesh@piedpiper.com',
+        },
+        {
+          id: 4,
+          name: 'Lando',
+          email: 'lando@piedpiper.com',
+        },
+      ]
+    }
+  },
+  methods: {
+    addEmployee(employee) {
+        const lastId =
+    this.employees.length > 0
+      ? this.employees[this.employees.length - 1].id
+      : 0;
+    const id = lastId + 1;
+    const newEmployee = { ...employee, id };
+
+      this.employees = [...this.employees, newEmployee]
+    }
   }
 }
 </script>
